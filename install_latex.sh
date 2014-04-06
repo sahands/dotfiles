244782813f7d553d18edc4ebe7ef234b432ab3a8
Sahand <sahands@gmail.com>
2014-04-06 16:14:38 -0700

    install latex added

---
+++ install_latex.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=`dirname $0`
+source $SCRIPT_DIR/include.sh
+
+ensure_root
+
+port_install texlive
+for P in basic bibtex-extra bin bin bin-extra common context fonts-recommended fonts-extra fontutils generic-recommended lang-english latex latex-extra latex-recommended luatex math-extra metapost pictures plain-extra pstricks science xetex
+do
+    port_install texlive-$P
+done
